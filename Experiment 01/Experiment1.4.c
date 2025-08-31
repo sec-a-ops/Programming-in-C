ff6eb932d097bf882c8e929b6986388e0c330deb
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-08-31 19:09:39 +0530

    Create Experiment1.4.c

---
+++ Experiment 01/Experiment1.4.c
@@ -0,0 +1,15 @@
+
+
+
+#include <stdio.h>
+#include<conio.h>
+ int main() {
+    
+    printf("*\n");
+    printf("***\n");
+    printf("*****\n");
+    printf("*******\n");
+    getch();
+
+    return 'a';
+}

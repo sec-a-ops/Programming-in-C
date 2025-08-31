ef3bb05471d3ea9be5bd91778f8d56e27050a778
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-08-31 19:03:22 +0530

    Create Experiment1.3.c

---
+++ Experiment 01/Experiment1.3.c
@@ -0,0 +1,47 @@
+/*Write a C program to perform four arithmetic operations on two variables. */
+#include <stdio.h>
+#include <conio.h>
+int main(){
+
+  
+  int a,b;
+    printf("enter the value a = "); 
+    scanf("%d",&a);
+    printf("enter the value b = "); 
+    scanf("%d",&b);
+    int add=0;
+    add = a + b;
+    printf("%d\n",add);
+    
+    int c,d;
+    printf("enter the value c = "); 
+    scanf("%d",&c);
+    printf("enter the value d = "); 
+    scanf("%d",&d);
+    int sub=0;
+    sub = c - d;
+    printf("%d\n",sub);
+    
+    int e,f;
+    printf("enter the value e = "); 
+    scanf("%d",&e);
+    printf("enter the value f = "); 
+    scanf("%d",&f);
+    int mul=0;
+    mul = e*f;
+    printf("%d\n",mul);
+    
+    int q,r;
+    printf("enter the value q = "); 
+    scanf("%d",&q);
+    printf("enter the value r = "); 
+    scanf("%d",&r);
+    int div=0;
+    div = q/r;
+    printf("%d\n",div);
+
+    getch();
+ 
+ 
+    return 0;
+}

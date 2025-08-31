8bdf089f1fec73f7f1a48607bad02f38e3e04dc9
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-08-31 19:01:46 +0530

    Update and rename lesson2.c to Experiment1.2.c

--- Experiment 01/Experiment1.2.c
+++ Experiment 01/Experiment1.2.c
@@ -1,10 +1,12 @@
 /*Experiment 1: installation, Environment setup and starting with C language
-4. Write a C program to add two numbers, take number from user. */
+4. Write a C program to add two numbers, and take a number from the user. */
 #include <stdio.h>
 #include<conio.h>
 int main()
 {
-    int a,b;//declaration
+    printf("Name: Ansh Bharadwaj\nSap-ID: 590029063\nCourse: Bsc CS\n Batch: 1st"); 
+    printf("-----------------------------");
+    int a,b;
     printf("enter the value a = "); 
     scanf("%d",&a);
     printf("enter the value b = "); 
@@ -19,3 +21,4 @@ int main()
     
     
         
+

766fb51127b1c95b303c3a32b4b0967526cdfe55
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-08-31 18:49:15 +0530

    Create Experiment1.2.c

---
+++ Experiment1.2.c
@@ -0,0 +1,23 @@
+/*Experiment 1: installation, Environment setup and starting with C language
+4. Write a C program to add two numbers and take a number from the user. */
+#include <stdio.h>
+#include<conio.h>
+int main()
+{
+  
+  
+  int a,b;//declaration
+    printf("enter the value a = "); 
+    scanf("%d",&a);
+    printf("enter the value b = "); 
+    scanf("%d",&b);
+    int sum=0;
+    sum = a + b;
+    printf("%d",sum);
+
+    getch();
+    return 0;
+}
+    
+    
+        

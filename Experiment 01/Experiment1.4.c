e53e4e5886a4f9bb4205008d042320e16724f596
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-08-31 19:11:40 +0530

    Update Experiment1.4.c

--- Experiment 01/Experiment1.4.c
+++ Experiment 01/Experiment1.4.c
@@ -1,10 +1,12 @@
-
+/*Experiment 1: installation, Environment setup and starting with C language
+2. Write a C program to print the address in different lines*/
 
 
 #include <stdio.h>
 #include<conio.h>
  int main() {
-    
+    printf("Name: Ansh Bharadwaj\nSap-ID: 590029063\nCourse: Bsc CS\n Batch: 1st"); 
+  printf("-----------------------------");
     printf("*\n");
     printf("***\n");
     printf("*****\n");

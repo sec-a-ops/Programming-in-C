0388291e319e7c7ebe6f531d654da7bf09214235
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-10-28 16:28:53 +0530

    Create Experiment9.3.c

---
+++ Experiment 09/Experiment9.3.c
@@ -0,0 +1,24 @@
+/*Experiment 6: Array 
+6.3. WAP to read a list of integers and store it in a single dimensional array. Write a C program to 
+f
+ ind the frequency of a particular number in a list of integers.*/ 
+#include <stdio.h> 
+#include <conio.h> 
+int main() { 
+printf("Name: Ansh Bharadwaj\nSap-ID: 590029063\nCourse: Bsc CS\n Batch: 1st\n");   
+printf("-----------------------------\n");  
+int n, i, num, count = 0; 
+printf("Enter number of elements: "); 
+scanf("%d", &n); 
+int arr[n]; 
+printf("Enter elements:\n"); 
+for (i = 0; i < n; i++) scanf("%d", &arr[i]); 
+printf("Enter number to find frequency: "); 
+scanf("%d", &num); 
+for (i = 0; i < n; i++) { 
+if (arr[i] == num) count++; 
+} 
+printf("Frequency of %d: %d\n", num, count); 
+getch(); 
+return 0; 
+} 

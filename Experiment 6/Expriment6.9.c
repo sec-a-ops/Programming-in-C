1806d503ebd26070e34ea71158f9b76073acb6e1
sec-a-ops <155834969+sec-a-ops@users.noreply.github.com>
2025-10-28 16:11:39 +0530

    Create Expriment6.9.c

---
+++ Experiment 6/Expriment6.9.c
@@ -0,0 +1,23 @@
+/* 4.3 Patterns: 9*/
+#include <stdio.h>
+int main()
+{
+ printf("Ansh Bharadwaj\nSAP ID: 5900290630\nCourse: BSc. CS.
+\nBatch: 1");
+ printf("\n-----------------\n");
+ printf("The pattern is:\n");
+ int i, j, k;
+ for (i = 1; i <= 5; i++) {
+ for (j = 5; j > i; j--) printf(" ");
+ for (k = 0; k < i; k++) printf("%d ", 5 - k);
+ for (k = 1; k < i; k++) printf("%d ", 5 - i + k + 1);
+ printf("\n");
+ }
+ for (i = 4; i >= 1; i--) {
+ for (j = 5; j > i; j--) printf(" ");
+ for (k = 0; k < i; k++) printf("%d ", 5 - k);
+ for (k = 1; k < i; k++) printf("%d ", 5 - i + k + 1);
+ printf("\n");
+ }
+ return 0;
+}
